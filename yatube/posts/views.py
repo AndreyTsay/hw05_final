6f79e86010a8496e1e83edf75b6608fb4c09f634
Андрей Цай <skaeris@yandex.ru>
2023-04-15 19:40:00 +0300

    add Cache

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.cache import cache_page
 
 from .forms import PostForm, CommentForm
 from .models import Post, Group, User, Comment
@@ -9,6 +10,7 @@ from .models import Post, Group, User, Comment
 NUMBER_OF_POSTS: int = 10
 
 
+@cache_page(20, key_prefix='index_page')
 def index(request):
     posts = Post.objects.select_related('group', 'author').all()
     paginator = Paginator(posts, NUMBER_OF_POSTS)
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -137,3 +137,8 @@ LOGIN_REDIRECT_URL = 'posts:index'
 CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+} 
